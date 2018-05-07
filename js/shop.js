70813f6c51ac897f2e430d34a630c18d66c36691
Aimee Zawacki <aimee@tna.digital>
2018-05-07 14:23:55 -0600

    all pages done, styling mostly done except for stretch goals. this is where you will revert to if youmess it all up :)

---
+++ js/shop.js
@@ -0,0 +1,76 @@
+let shop = [
+  {
+    name: 'Flower Power',
+    type: 'mothers',
+    description: 'Hand painted card by Steph Stout for MESA - Moving to End Sexual Assault',
+    price: 5,
+    img: 'img/shop/flowers.jpg'
+  },
+
+  {
+    name: 'Live Life',
+    type: 'thinking',
+    description: 'Hand drawn type by Elle Levy for MESA — Moving to End Sexual Assault',
+    price: 5,
+    img: 'img/shop/palm.jpg'
+  },
+
+  {
+    name: 'You Are Special',
+    type: 'birthday',
+    description: '3D card perfect for your pastry loving bff. Collaged by Michelle Merlin for MESA',
+    price: 5,
+    img: 'img/shop/donuts.jpg'
+  },
+
+  {
+    name: 'Merry',
+    type: 'holiday',
+    description: 'Simple non-denominational holiday card by Steph Stout for MESA',
+    price: 5,
+    img: 'img/shop/love.jpg'
+  },
+
+  {
+    name: 'Wildflowers',
+    type: 'thinking',
+    description: 'Lovely hand painted wildflowers for your hippie friend, lover or sibling. By Optimystic for MESA',
+    price: 5,
+    img: 'img/shop/flower2.jpg'
+  },
+
+  {
+    name: 'Christmas',
+    type: 'holiday',
+    description: 'Classic Christmas card with gold leaf lettering By Optimystic for MESA',
+    price: 5,
+    img: 'img/shop/xmas1.jpg'
+  },
+
+  {
+    name: 'Mom',
+    type: 'mothers',
+    description: 'Cut paper card for your hella special mother by Elle Levy for MESA',
+    price: 5,
+    img: 'img/shop/mom.png'
+  },
+
+  {
+    name: 'Cuando LLueva',
+    type: 'birthday',
+    description: "A Birthday isn't always a joyous occasion. By Elle Levy for MESA",
+    price: 5,
+    img: 'img/shop/script.jpg'
+  }
+]
+
+document.addEventListener('DOMContentLoaded', function() {
+
+let shopCard = document.createElement()
+
+});
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
